9c28be6ed038d09b1162180843c642a01731754b
Jesse <54542639+jeb495@users.noreply.github.com>
2021-02-08 10:29:22 -0800

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,9 +1,9 @@
 //cheerio & request
 const request = require('request');
 const cheerio = require('cheerio');
-var url = ('https://hungry-carson-df6d18.netlify.app/');
+var url = ('https://www.google.com/');
 if (url != null){
-request('https://hungry-carson-df6d18.netlify.app/', (error, response, html) => {
+request('https://www.google.com/', (error, response, html) => {
 if(!error && response.statusCode == 200){
 const $ = cheerio.load(html);
 const siteHeading = $('<head>...</head>');
@@ -27,7 +27,7 @@ const takescreenshot = async () => {
 
     };    
     
-    await page.goto('https://natebennettsoundboard.tk/');
+    await page.goto('https://www.google.com/');
     await page.screenshot(options);
     await browser.close();
 };
